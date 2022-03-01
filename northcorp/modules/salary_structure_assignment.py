a9d25e891b2b2e76e8853987edd9f6aa341f46b8
Shahzad Naser <shahzadnaser1122@gmail.com>
2022-03-01 22:31:21 +0500

    update calculation

--- northcorp/modules/salary_structure_assignment.py
+++ northcorp/modules/salary_structure_assignment.py
@@ -2,10 +2,14 @@
 # License: GNU General Public License v3. See license.txt
 
 import frappe
+from frappe.utils import flt
 
 def before_save(doc, method):
     if doc.base:
         emp_working_hours = frappe.db.get_value("Employee",doc.employee,'working_hours') or 8
         emp_working_days = frappe.db.get_value("Department",doc.department,'working_days') or 30
-
-        doc.hourly_rate = round(doc.base/(emp_working_days*emp_working_hours), 2)
+        total_salary = flt(doc.base)
+        for row in doc.employee_salary_components:
+            if row.hourly:
+                total_salary += flt(row.amount)
+        doc.hourly_rate = round(total_salary/(emp_working_days*emp_working_hours), 2)
